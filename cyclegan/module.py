9c77b2ed44e85e4be510c7402712bea37bb92942
Mikołaj Soska <mikolaj.soska1@gmail.com>
2023-11-24 19:01:13 +0100

    [PP-4] Added method for validation step.

--- cyclegan/module.py
+++ cyclegan/module.py
@@ -50,6 +50,81 @@ class CycleGAN(pl.LightningModule):
         self.discriminator_a = Discriminator(in_channels)  # Is A real
         self.discriminator_b = Discriminator(in_channels)  # Is B real
 
+    def __run_generators(
+        self, image_a: torch.Tensor, image_b: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Method for running single generation step in training or validation.
+
+        Parameters
+        ----------
+        image_a : torch.Tensor
+            Image from the first domain
+        image_b : torch.Tensor
+            Image from the second domain
+
+        Returns
+        -------
+        torch.Tensor
+            Generator loss
+        torch.Tensor
+            Transformed image B (B -> A)
+        torch.Tensor
+            Transformed image A (A -> B)
+        """
+        generated_a = self.generator_b(image_b)  # B (real) -> A (fake)
+        generated_b = self.generator_a(image_a)  # A (real) -> B (fake)
+        cycle_a = self.generator_b(generated_b)  # B (fake) -> A (fake)
+        cycle_b = self.generator_a(generated_a)  # A (fake) -> B (fake)
+        cycle_loss = nn.functional.l1_loss(cycle_a, image_a) + nn.functional.l1_loss(cycle_b, image_b)
+
+        identity_a = self.generator_b(image_a)  # A (real) -> A (fake)
+        identity_b = self.generator_a(image_b)  # B (real) -> B (fake)
+        identity_loss = nn.functional.l1_loss(identity_a, image_a) + nn.functional.l1_loss(identity_b, image_b)
+
+        generator_loss = cycle_loss + identity_loss
+
+        return generator_loss, generated_a, generated_b
+
+    def __run_discriminator(
+        self, real_a: torch.Tensor, real_b: torch.Tensor, fake_a: torch.Tensor, fake_b: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Method for running single discriminator step in training or validation.
+
+        Parameters
+        ----------
+        real_a : torch.Tensor
+            Image from the first domain
+        real_b : torch.Tensor
+            Image from the second domain
+        fake_a : torch.Tensor
+            Fake image from the first domain (B -> A)
+        fake_b : torch.Tensor
+            Fake image from the second domain (A -> B)
+
+        Returns
+        -------
+        torch.Tensor
+            Discriminator loss
+        """
+        prediction_fake_a = self.discriminator_a(fake_a)
+        prediction_fake_b = self.discriminator_b(fake_b)
+        prediction_real_a = self.discriminator_a(real_a)
+        prediction_real_b = self.discriminator_b(real_b)
+
+        target_fake = torch.zeros_like(prediction_fake_a)
+        target_real = torch.ones_like(prediction_real_a)
+
+        discriminator_loss = 0.5 * (
+            nn.functional.mse_loss(prediction_fake_a, target_fake)  # Fake image A
+            + nn.functional.mse_loss(prediction_real_a, target_real)  # Real image A
+            + nn.functional.mse_loss(prediction_fake_b, target_fake)  # Fake image B
+            + nn.functional.mse_loss(prediction_real_b, target_real)  # Real image B
+        )
+
+        return discriminator_loss
+
     def training_step(self, batch: list[torch.Tensor], batch_index: int) -> None:
         """
         Method defines single training step of CycleGAN.
@@ -71,18 +146,7 @@ class CycleGAN(pl.LightningModule):
 
         # Generators training
         self.toggle_optimizer(optimizer_generator)
-
-        generated_a = self.generator_b(image_b)  # B (real) -> A (fake)
-        generated_b = self.generator_a(image_a)  # A (real) -> B (fake)
-        cycle_a = self.generator_b(generated_b)  # B (fake) -> A (fake)
-        cycle_b = self.generator_a(generated_a)  # A (fake) -> B (fake)
-        cycle_loss = nn.functional.l1_loss(cycle_a, image_a) + nn.functional.l1_loss(cycle_b, image_b)
-
-        identity_a = self.generator_b(image_a)  # A (real) -> A (fake)
-        identity_b = self.generator_a(image_b)  # B (real) -> B (fake)
-        identity_loss = nn.functional.l1_loss(identity_a, image_a) + nn.functional.l1_loss(identity_b, image_b)
-
-        generator_loss = cycle_loss + identity_loss
+        generator_loss, generated_a, generated_b = self.__run_generators(image_a, image_b)
         self.manual_backward(generator_loss)
 
         optimizer_generator.step()
@@ -91,26 +155,31 @@ class CycleGAN(pl.LightningModule):
 
         # Discriminators training
         self.toggle_optimizer(optimizer_discriminator)
-
-        prediction_fake_a = self.discriminator_a(generated_a.detach())
-        prediction_fake_b = self.discriminator_b(generated_b.detach())
-        prediction_real_a = self.discriminator_a(image_a)
-        prediction_real_b = self.discriminator_b(image_b)
-
-        target_fake = torch.zeros_like(prediction_fake_a)
-        target_real = torch.ones_like(prediction_real_a)
-
-        discriminator_loss = 0.5 * (
-            nn.functional.mse_loss(prediction_fake_a, target_fake)  # Fake image A
-            + nn.functional.mse_loss(prediction_real_a, target_real)  # Real image A
-            + nn.functional.mse_loss(prediction_fake_b, target_fake)  # Fake image B
-            + nn.functional.mse_loss(prediction_real_b, target_real)  # Real image B
-        )
+        discriminator_loss = self.__run_discriminator(image_a, image_b, generated_a.detach(), generated_b.detach())
         self.manual_backward(discriminator_loss)
 
         optimizer_discriminator.step()
         optimizer_discriminator.zero_grad()
         self.untoggle_optimizer(optimizer_discriminator)
+
+    def validation_step(self, batch: list[torch.Tensor], batch_index: int) -> None:
+        """
+        Method defines single validation step of CycleGAN.
+
+        Parameters
+        ----------
+        batch : list[torch.Tensor]
+            List of length = 2, that holds single batch of images from both domains
+        batch_index : int
+            Index number of given batch
+
+        Returns
+        -------
+        None
+        """
+        image_a, image_b = batch
+        _, generated_a, generated_b = self.__run_generators(image_a, image_b)
+        self.__run_discriminator(image_a, image_b, generated_a, generated_b)
 
     def configure_optimizers(self) -> list[torch.optim.Optimizer]:
         """
