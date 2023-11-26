96176155ace2f0032408ad85c15a965114254596
Mikołaj Soska <mikolaj.soska1@gmail.com>
2023-11-26 18:06:02 +0100

    [PP-4] Added training configuration with hydra library.

--- cyclegan/train.py
+++ cyclegan/train.py
@@ -1,5 +1,7 @@
-import typing
-
+# noinspection PyPackageRequirements
+# This package is installed as hydra-core, PyCharm doesn't recognize this
+import hydra
+import omegaconf
 import pytorch_lightning as pl
 import torchvision
 
@@ -7,34 +9,41 @@ from cyclegan import CycleGAN
 from cyclegan.data.module import CycleGANDataModule
 
 
-def train(
-    dataset_type: typing.Literal["celeba", "custom"],
-    dataset_root: str,
-    feature: str = None,
-    test_size: float = 0.3,
-    image_size: int = 128,
-    seed: int | None = 0,
-    max_epochs: int = 3,
-) -> None:
+@hydra.main(config_path="../configs", config_name="train.yaml", version_base=None)
+def train(config: omegaconf.DictConfig) -> None:
+    """
+    Runs CycleGAN training with parameters from config.
+
+    Parameters
+    ----------
+    config : omegaconf.DictConfig
+        Training config read from YAML file
+
+    Returns
+    -------
+    None
+    """
     datamodule = CycleGANDataModule(
-        dataset_type=dataset_type,
-        dataset_root=dataset_root,
-        test_size=test_size,
-        feature=feature,
+        dataset_type=config.dataset.type,
+        dataset_root=config.dataset.root_dir,
+        test_size=config.dataset.test_size,
+        feature=config.dataset.feature,
         transform=torchvision.transforms.Compose(
-            [torchvision.transforms.Resize((image_size, image_size)), torchvision.transforms.ToTensor()]
+            [
+                torchvision.transforms.Resize((config.dataset.image_size, config.dataset.image_size)),
+                torchvision.transforms.ToTensor(),
+            ]
         ),
-        seed=seed,
+        seed=config.training.seed,
     )
 
-    model = CycleGAN()
-    trainer = pl.Trainer(max_epochs=max_epochs)
+    model = CycleGAN(
+        residual_blocks=config.model.residual_blocks,
+        learning_rate=config.training.learning_rate,
+    )
+    trainer = pl.Trainer(max_epochs=config.training.max_epochs)
     trainer.fit(model, datamodule)
 
 
-def main() -> None:
-    train(dataset_type="celeba", dataset_root="data", feature="Smiling")
-
-
 if __name__ == "__main__":
-    main()
+    train()
